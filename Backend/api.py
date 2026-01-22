21f2a4debb1952390a41d7d2f85185fde81a8546
Gustavo Marrufo <gustavitotlgac@gmail.com>
2026-01-21 22:54:07 -0600

    Nueva version en desarrollo

--- Backend/api.py
+++ Backend/api.py
@@ -228,4 +228,4 @@ def procesar_descarga_generica(req, tipo):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
